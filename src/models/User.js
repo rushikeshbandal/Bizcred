33a2762d621e231dddf4c4b3157c196dd560f4ae
Nandita Madkunti <madkuntinandita@gmail.com>
2026-07-03 12:41:56 +0530

    commit all changes

--- src/models/User.js
+++ src/models/User.js
@@ -4,11 +4,6 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-<<<<<<< HEAD
-creditScore: {type: Number,default: 0},
-=======
-
->>>>>>> origin/main
   role: {
     type: String,
     enum: ["admin", "user"],
@@ -22,30 +17,6 @@ creditScore: {type: Number,default: 0},
   },
 
   // ✅ UPDATED KYC
-<<<<<<< HEAD
- kyc: {
-  pan: String,
-
-  aadhaar: String,
-
-  sessionId: String,
-
-  status: {
-    type: String,
-    enum: [
-      "pending",
-      "approved",
-      "rejected",
-      "in_progress",
-      "in_review"
-    ],
-    default:  "not_submitted"
-  },
-
-  verificationData: mongoose.Schema.Types.Mixed,
-
-  updatedAt: Date
-=======
 kyc: {
   pan: String,
   aadhaar: String,
@@ -59,7 +30,6 @@ kyc: {
     enum: ["not_submitted", "pending", "approved", "rejected"],
     default: "not_submitted"
   }
->>>>>>> origin/main
 }
 
 }, { timestamps: true });
